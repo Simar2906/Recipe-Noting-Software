068fcb44c6700fbcdd404c013cbb1e051c62d0a2
Simar2906 <simar2906@gmail.com>
2024-05-31 22:27:54 +0530

    Fetch on main page

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+
 var checkCreations = function(){
     // alert('Creating local data structuures');
     if(localStorage.getItem('Recipes') == null){
-        localStorage.setItem('Recipes', JSON.stringify([]));
+        localStorage.setItem('Recipes', JSON.stringify({}));
     }
     if(localStorage.getItem('Indices') == null){
         localStorage.setItem('Indices', JSON.stringify({}));
@@ -25,6 +26,36 @@ var updateData = function(jsonData){
     localStorage.setItem('Indices', JSON.stringify(indices));
 
     let recipes = JSON.parse(localStorage.getItem('Recipes'));
-    recipes.push(jsonData);
+    recipes[jsonData.Recipe_Name] = jsonData;
+    // recipes.push(jsonData);
     localStorage.setItem('Recipes', JSON.stringify(recipes));
 }
+
+var fetchRecipes = function(){
+    var message = '';
+    var indices = JSON.parse(localStorage.getItem('Indices'));
+    var recipeDetails = JSON.parse(localStorage.getItem('Recipes'));
+    console.log(recipeDetails);
+    var recipes = Object.keys(indices);
+    if(recipes.length != 0){
+        recipes.forEach(recipe => {
+            var recipeName = indices[recipe];
+            message += `<div class="recipeCard" click>
+                        
+                        <div class="recipeCardName">${recipeName}</div>
+                        <img class="recipeCardImg" src="${recipeDetails[recipeName].Image_URL}" alt="Image of ${recipeDetails[recipeName].Recipe_Name}"></img>
+
+                        </div>`;
+                        // <button>&#9998;</button>
+                        // <button>&#88;</button>
+            console.log(message);
+        });
+    }
+    else{
+        message = '<p>Oops! No Recipes</p>';
+    }
+
+    document.getElementById('RecipeContainer').innerHTML = message;
+}
+
+fetchRecipes();
